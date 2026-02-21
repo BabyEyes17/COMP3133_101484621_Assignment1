dacb7271123025acd24142137a1e9b9849e0db68
Jayden Lewis <jaydent.lewis@gmail.com>
2026-02-21 02:46:23 -0500

    Implemented secure connection to MongoDB Atlas and commented code in server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const { ApolloServer, gql } = require("apollo-server-express");
-
 const app = express();
 
+
+
+// Graphql testing
 const typeDefs = gql`
     
     type Query {
@@ -19,11 +22,34 @@ const resolvers = {
     },
 };
 
+
+
+// Starting the server
 async function startServer() {
     
+
+
+    // Connecting to MongoDB
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+        throw new Error("MONGO_URI is missing from the .env file.")
+    }
+
+    await mongoose.connect(uri)
+    console.log("Connected to MongoDB Atlas.")
+
+
+
+    // Starting Apollo
     const server = new ApolloServer({ typeDefs, resolvers });
     await server.start();
     server.applyMiddleware({ app });
+
+
+
+    // Starting Express
+    const port = process.env.port || 4000;
 
     app.listen(4000, async () => {
     
@@ -36,4 +62,8 @@ async function startServer() {
     });
 }
 
-startServer();
+
+
+startServer().catch((err) => {
+    console.error("Startup error:", err.message);
+});
